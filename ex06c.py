768b5992c3b60ad553fbc297aa9f4b16ed301c50
Sanela Becovic <sanela.becovic@udg.edu.me>
2019-02-15 17:36:08 +0100

    ex06c.py

---
+++ ex06c.py
@@ -0,0 +1,9 @@
+# ex06c.py
+
+def main():
+    print("This program illustrates the chaootic function")
+    x = eval(input("Enter number between 0 and 1: "))
+    for i in range(100):
+        x = 3.9 * x - 3.9 * x *x
+        print(x)
+main()
