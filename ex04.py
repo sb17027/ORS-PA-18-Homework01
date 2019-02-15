7c695ebe906301a9c2c572ff075cefc9925731ec
Sanela Becovic <sanela.becovic@udg.edu.me>
2019-02-15 16:53:29 +0100

    ex04.py

---
+++ ex04.py
@@ -0,0 +1,9 @@
+# ex04.py
+
+def main():
+    print("The program illustrates a chaotic function")
+    x = eval(input("Enter a number between 0 and 1: "))
+    for i in range(20):
+        x = 3.9 * x * (1-x)
+        print(x)
+main()
